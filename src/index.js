91cb347f7065a53772aba05e5f779a78708077fb
avalonprod <abramenkoroman22@gmail.com>
2023-11-18 20:25:16 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.html'
+import './index.css'
+import './styles/somthing.css'
+import { sum } from './modules/somthing'
+// Your code
+console.log(sum(3, 5))
---
+++ src/modules/somthing.js
@@ -0,0 +1,3 @@
+console.log("somthing string")
+
+export const sum = (a, b) => a + b
